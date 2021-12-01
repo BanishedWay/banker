dae61b33e4a8df51f845f93ca2b6aab7505ea151
Michael-cybersecurity <xjq20011023@163.com>
2021-12-02 07:46:01 +0800

    优化代码逻辑

---
+++ build/CMakeFiles/banker.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/15573/Documents/vscode/cmake/banker/banker.cpp" "CMakeFiles/banker.dir/banker.cpp.obj" "gcc" "CMakeFiles/banker.dir/banker.cpp.obj.d"
+  "C:/Users/15573/Documents/vscode/cmake/banker/main.cpp" "CMakeFiles/banker.dir/main.cpp.obj" "gcc" "CMakeFiles/banker.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/banker.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/banker.dir/banker.cpp.obj"
+  "CMakeFiles/banker.dir/banker.cpp.obj.d"
+  "CMakeFiles/banker.dir/main.cpp.obj"
+  "CMakeFiles/banker.dir/main.cpp.obj.d"
+  "banker.exe"
+  "banker.exe.manifest"
+  "banker.pdb"
+  "libbanker.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/banker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
